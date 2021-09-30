6d2a7818aa43517214e41aea4cf1ec7223ca0c62
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-09-30 20:59:16 +0300

    add routes, models, controllers, middlewares, db, utils

---
+++ homework-02/model/contacts/index.js
@@ -0,0 +1,13 @@
+const getAllContacts = require("./getAllContacts");
+const getContactById = require("./getContactById");
+const removeContactById = require("./removeContactById");
+const addContact = require("./addContact");
+const updateContactById = require("./updateContactById");
+
+module.exports = {
+  getAllContacts,
+  getContactById,
+  removeContactById,
+  addContact,
+  updateContactById,
+};
