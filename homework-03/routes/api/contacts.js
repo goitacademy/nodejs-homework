c94a7f9c0e5d39732af5a6095e1743c71cb6c0ed
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-10-03 12:13:08 +0300

    replace local connection to mangoose connection to mongoDB and add method patch

--- homework-03/routes/api/contacts.js
+++ homework-03/routes/api/contacts.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
-const { contacts: ctrl } = require('../../controllers/contacts');
+const {
+	joiContactSchema,
+	updateFavoriteContactJoiSchema,
+} = require('../../models/contacts/contactSchema');
 const { controllerWrapper, validation } = require('../../middlewares');
-const { contactSchema } = require('../../schemas');
+const { contacts: ctrl } = require('../../controllers/contacts');
 
 const router = express.Router();
 
@@ -10,14 +13,23 @@ router.get('/', controllerWrapper(ctrl.getAllContacts));
 
 router.get('/:id', controllerWrapper(ctrl.getContactById));
 
-router.post('/', validation(contactSchema), controllerWrapper(ctrl.addContact));
+router.post(
+	'/',
+	validation(joiContactSchema),
+	controllerWrapper(ctrl.addContact),
+);
 
 router.put(
 	'/:id',
-	validation(contactSchema),
+	validation(joiContactSchema),
 	controllerWrapper(ctrl.updateContactById),
 );
 
+router.patch(
+	'/:id',
+	validation(updateFavoriteContactJoiSchema),
+	controllerWrapper(ctrl.updateFavoriteContact),
+);
 router.delete('/:id', controllerWrapper(ctrl.removeContactById));
 
 module.exports = router;
