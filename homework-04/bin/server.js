57a9f9c3e068281f11116cae6faa65f7ffc017f8
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-10-05 15:15:20 +0300

    change DB_HOST variable

--- homework-04/bin/server.js
+++ homework-04/bin/server.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const app = require('../app');
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_USER, DB_USER_PASS, DB_NAME, PORT = 3000 } = process.env;
+const DB_HOST = `mongodb+srv://${DB_USER}:${DB_USER_PASS}@cluster0.8pzsd.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+// console.log(DB_HOST);
 
 mongoose
 	.connect(DB_HOST)
